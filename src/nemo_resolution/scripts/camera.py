27ffd739d894c4a065f0180362c9eaf0a1614f18
wagaojr <wagnerjunior@poli.ufrj.br>
2022-04-14 22:46:09 -0300

    camera att

--- src/nemo_resolution/scripts/camera.py
+++ src/nemo_resolution/scripts/camera.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+# Projeto Final Nautilus
+# Helena e Wagner
 import cv2
 import numpy as np
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
 from marlintwist import Andar
 
-rospy.init_node("testando", anonymous=True)
+rospy.init_node("nadar", anonymous=True)
 
 class Camera():
     def __init__(self):
@@ -17,7 +21,6 @@ class Camera():
         greenLower = (29, 86, 6)
         greenUpper = (64, 255, 255)
         rate = rospy.Rate(10)
-        #msg = rospy.wait_for_message("camera/image_raw", Image)
         self.img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         self.hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
         maskGreen = cv2.inRange(self.hsv, greenLower, greenUpper)
@@ -25,11 +28,14 @@ class Camera():
         maskGreen = cv2.dilate(maskGreen, None, iterations=2)
         cntGreen = cv2.findContours(maskGreen.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
         
-        if len(cntGreen) > 0:
-            marlin = Andar()
-            marlin.start()
+        while cntGreen == 0:
+            self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+            velocidade = Twist()
+            velocidade.linear.y = 0
+            velocidade.angular.z = 1
+            self.pub.publish(velocidade)
+            rate.sleep()
 
-        rate.sleep()
 
 if __name__ == '__main__':
     try:
